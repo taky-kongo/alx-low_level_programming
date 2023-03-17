0d016eb07b7d5ecf2f100c2b15e70bc7c26a8dc9
Malick Kongo <abdoulmalickkongo@gmail.com>
2023-03-17 02:52:21 +0000

    add the file 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - prints a message
  *
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
 	return (1);
 }
