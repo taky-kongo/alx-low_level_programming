d6b709b6eb987a917c86c93d4c8126a64b51c80e
Malick Kongo <abdoulmalickkongo@gmail.com>
2023-03-17 23:58:20 +0000

    Add the file 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,14 +8,14 @@
 int main(void)
 {
 	int number = 0;
-	char number_al = 'A';
+	char number_al = 'a';
 
 	while (number <= 9)
 	{
 		putchar(number + '0');
 		number++;
 	}
-	while (number_al <= 'F')
+	while (number_al <= 'f')
 	{
 		putchar(number_al + 0);
 		number_al++;
