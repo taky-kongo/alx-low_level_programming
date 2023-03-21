c77663faf198e1fd2c7a24333a59796942c6f59d
Malick Kongo <abdoulmalickkongo@gmail.com>
2023-03-21 22:48:24 +0000

    Add the files main.h and 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - a function that checks for alphabetic character.
+ *
+ * @c: The argument to enter
+ *
+ * Return: 1 if c is a letter, lowercase or uppercase.
+ */
+int _isalpha(int c)
+{
+	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+		return (1);
+	else
+		return (0);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -13,3 +13,9 @@
 	* Prototype: int _islower(int c);
 	* Returns 1 if c is lowercase
 	* Returns 0 otherwise
+
+4-isalpha.c: This program is a function that checks for alphabetic character.
+
+	* Prototype: int _isalpha(int c);
+	* Returns 1 if c is a letter, lowercase or uppercase
+	* Returns 0 otherwise
