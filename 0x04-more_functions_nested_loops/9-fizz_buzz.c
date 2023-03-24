7171fcd9ef4f419792c6505cf0b85d178c7e594a
Malick Kongo <abdoulmalickkongo@gmail.com>
2023-03-24 11:49:05 +0000

    Add the file 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main -  prints the numbers from 1 to 100
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 15) == 0)
+			printf("FizzBuzz ");
+		if ((i % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((i % 5) == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
