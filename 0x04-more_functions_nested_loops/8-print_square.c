91fcaf5bfda706b291c4c54c3c9eeb71ced718d6
Malick Kongo <abdoulmalickkongo@gmail.com>
2023-03-24 00:47:54 +0000

    Add the files main.h and 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,20 +9,19 @@ void print_square(int size)
 {
 	int i, j;
 
-	if (size <= 0)
-	{
 
-	}
-	else
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
+		for (j = 0; j < size; j++)
 		{
-			for (j = 0; j < size; j++)
-			{
+			if (size > 0)
 				_putchar('#');
+			else
+			{
+
 			}
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 	_putchar('\n');
 }
