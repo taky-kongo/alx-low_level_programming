851a9f5ca49778b523d9968388661e402908667a
Malick Kongo <abdoulmalickkongo@gmail.com>
2023-03-28 16:34:49 +0000

    Update the file 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,13 +13,8 @@ void print_rev(char *s)
 		j++;
 	while (j > 0 || ((*(s + j)) != '\0'))
 	{
-		if (((*(s + j)) == '\0'))
-		{
-
-		}
 		_putchar((*(s + j)));
 		j--;
 	}
-	_putchar('\0');
 	_putchar('\n');
 }
