26b5fea7f76749012073cb4974ce5def34b9a951
Malick Kongo <abdoulmalickkongo@gmail.com>
2023-03-29 23:49:26 +0000

    Add the files 0-strcat.c and main.h

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strcat - This function appends the src string to the dest string.
+ *
+ * @dest: The string that receives
+ * @src: The string to add
+ *
+ * Return: a pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+	int k, l;
+
+	while (*dest != '\0')
+	{
+		i++;
+		dest++;
+	}
+	while (*(src + j) != '\0')
+	{
+		j++;
+	}
+	j++;
+	l = i + j;
+	for (k = i; k < l; k++)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	return (dest);
+}
