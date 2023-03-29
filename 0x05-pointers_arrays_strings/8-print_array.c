0e297a0f1aa78b708e093423b64961b9a1eee609
Malick Kongo <abdoulmalickkongo@gmail.com>
2023-03-29 09:50:25 +0000

    Add the files main.h and 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,11 +14,8 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", *(a + i));
-		if (i != n)
-		{
-			putchar(',');
-			putchar(' ');
-		}
+		if (i != (n - 1))
+			printf(", ");
 	}
-	putchar('\n');
+	printf("\n");
 }
