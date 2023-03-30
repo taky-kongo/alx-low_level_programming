09d1d507af77be81f75ab1beab55682763f01a91
Malick Kongo <abdoulmalickkongo@gmail.com>
2023-03-30 14:44:18 +0000

    Add the files 2-strncpy.c and main.h

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _strncpy - a function that copies a string.
+ *
+ * @dest: The string that receives
+ * @src: The string to copy
+ * @n: the number of src bytes
+ *
+ * Return: a pointer to the resulting string dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+	int k;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		j++;
+	}
+	if (n <= j)
+	{
+		for (k = 0; k < n; k++)
+		{
+			dest[k] = src[k];
+		}
+	}
+	else
+	{
+		for (k = 0; k < i; k++)
+		{
+			dest[k] = src[k];
+		}
+	}
+	dest[i] = '\0';
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -11,3 +11,8 @@
 	* it will use at most n bytes from src; and
 	* src does not need to be null-terminated if it contains n or more bytes
 	* Return a pointer to the resulting string dest
+
+2-strncpy.c: This program is a function that copies a string.
+
+	* Prototype: char *_strncpy(char *dest, char *src, int n);
+	* Your function should work exactly like strncpy
