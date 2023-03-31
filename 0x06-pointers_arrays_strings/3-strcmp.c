1e8f384f4968cc956a2ec50ba7aa0e5a5ee9278c
Malick Kongo <abdoulmalickkongo@gmail.com>
2023-03-31 00:47:00 +0000

    Update the file 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,10 +15,21 @@ int _strcmp(char *s1, char *s2)
 	int i = 0;
 	int j = 0;
 
-	if (s1[i] > s2[j])
-		return (s1[i] - s2[j]);
-	else if (s1[i] < s2[j])
-		return (s1[i] - s2[j]);
-	else
-		return (0);
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] > s2[i])
+		{
+			j = (s1[i] - s2[i]);
+			break;
+		}
+		else if (s1[i] < s2[i])
+		{
+			j = (s1[i] - s2[i]);
+			break;
+		}
+		else
+		j = 0;
+	i++;
+	}
+	return (j);
 }
