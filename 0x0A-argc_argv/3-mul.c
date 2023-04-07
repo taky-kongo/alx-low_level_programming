fadadae33b3081b3d8ae14f258a931210140c220
Malick Kongo <abdoulmalickkongo@gmail.com>
2023-04-07 17:59:44 +0000

    Add the file 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers.
+ *
+ * @argc: The number of command line arguments
+ * @argv: The array
+ *
+ * Return: 0 success
+ * 1 error
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 1;
+	int i;
+
+	if (argc <= 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			sum = sum * atoi((argv[i]));
+		}
+		printf("%d\n", sum);
+
+	}
+	return (0);
+}
