ed020fd5b955539c3eff341b6047ec9021b29730
Malick Kongo <abdoulmalickkongo@gmail.com>
2023-04-07 16:35:14 +0000

    add the file 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program, followed by a new line.
+ *
+ * @argc: the number of elements in the array argv
+ * @argv: the array
+ *
+ * Return: Always 0.
+ */
+int main(int __attribute__ ((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
