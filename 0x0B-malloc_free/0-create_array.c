8bd457148c2bf34fd768021e1c670e8ff7d020d3
Malick Kongo <abdoulmalickkongo@gmail.com>
2023-04-11 14:29:23 +0000

    Add the files 0-create_array.c and main.h

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,6 +16,10 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	str = malloc(sizeof(char) * size);
+	if (str == NULL)
+	{
+		return ('\0');
+	}
 	if (size == '\0')
 	{
 		return ('\0');
