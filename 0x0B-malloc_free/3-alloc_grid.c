6870ffb464ae08c5e8376512963ebbd8e5e8bcc3
Malick Kongo <abdoulmalickkongo@gmail.com>
2023-04-12 12:24:07 +0000

    Update the file 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,7 +20,7 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	tab = malloc(sizeof(int) * height);
+	tab = (int **)malloc(sizeof(int *) * height);
 	if (tab == NULL)
 	{
 		return (NULL);
