491a06a77bca1fbd5dadcda56dfc0dc292a1f561
Malick Kongo <abdoulmalickkongo@gmail.com>
2023-04-18 17:22:29 +0000

    Add the file dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,5 @@
+dog.h: This program defined a new type struct dog with the following elements:
+
+	* name, type = char *
+	* age, type = float
+	* owner, type = char *
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - the dog's information
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: the dog's information
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
