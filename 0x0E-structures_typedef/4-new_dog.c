5fb8d95216412b80a5252cba09bd23af6622a4f3
taky-kongo <abdoulmalickkongo@gmail.com>
2023-04-24 17:05:01 +0000

    update the file 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,15 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+/**
+ * new_dog - a function that creates a new dog.
+ *
+ * @name: name of the new dog
+ * @age: age
+ * @owner: owner
+ *
+ * Return: NULL if the function fails
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
