61f71640c3eef7b37037c819fd8f110d3cc0adb5
taky-kongo <abdoulmalickkongo@gmail.com>
2023-04-26 23:16:16 +0000

    Update the file 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,8 +12,10 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	long unsigned int i;
+	unsigned int i;
 
+	if (array == NULL || action == NULL)
+		return;
 	for (i = 0; i < size; i++)
 	{
 		(*action)(array[i]);
