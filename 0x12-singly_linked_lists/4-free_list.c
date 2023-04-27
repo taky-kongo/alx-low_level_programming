1ce8bb95bd1854836689c9be54ba23220ca1b17d
taky-kongo <abdoulmalickkongo@gmail.com>
2023-04-27 23:32:03 +0000

    Add the file 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * free_list - a function that frees a list_t list
+ *
+ * @head: a list to enter
+ */
+void free_list(list_t *head)
+{
+	if (head == NULL)
+		return (NULL);
+	if (head->next == NULL)
+	{
+		free(head);
+		head = NULL;
+	}
+}
