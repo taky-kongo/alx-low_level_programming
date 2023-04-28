02ae5055a0f756ab682e0205401b7aaaec6b6b36
taky-kongo <abdoulmalickkongo@gmail.com>
2023-04-28 16:43:16 +0000

    Update the file 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include "lists.h"
 
+void first(void) __attribute__((constructor));
 /**
  * first - a function that prints before the main function is executed.
  */
 void first(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-			I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
