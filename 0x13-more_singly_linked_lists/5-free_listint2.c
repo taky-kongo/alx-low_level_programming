4d8a8a2f5b0767e9c0e1bbdbbc8f82d73ca3e4a7
taky-kongo <abdoulmalickkongo@gmail.com>
2023-05-03 12:10:42 +0000

    Add the files 5-free_listint2.c and lists.h

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list.
+ *
+ * @head: A node
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *clear;
+	listint_t *temp;
+
+	temp = *head;
+	while (temp != NULL)
+	{
+		clear = temp;
+		temp = temp->next;
+		free(clear);
+	}
+	*head = NULL;
+}
