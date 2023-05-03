d7bb0045941be82fe8011e3dc6ca88326744b657
taky-kongo <abdoulmalickkongo@gmail.com>
2023-05-03 23:15:10 +0000

    Update the file 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,7 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
-	while (node && index != idx)
+	while (node && index != idx - 1)
 	{
 		index++;
 		node = node->next;
