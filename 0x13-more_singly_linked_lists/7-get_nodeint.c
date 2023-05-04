ea920cf18ae153624b25ed84c356a1ae876d2bbf
taky-kongo <abdoulmalickkongo@gmail.com>
2023-05-04 18:05:28 +0000

    update the file 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -15,10 +15,18 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i = 0;
 
 
-	for (; i < index && head != NULL; i++)
-	{
-		head = head->next;
-	}
-
+	/**
+	 * for (; i < index && head != NULL; i++)
+	 * {
+	 *	head = head->next;
+	 * }
+	 *
+	 * return (head);
+	 */
+	while (head != NULL && i < index)
+        {
+                i++;
+                head = head->next;
+        }
 	return (head);
 }
