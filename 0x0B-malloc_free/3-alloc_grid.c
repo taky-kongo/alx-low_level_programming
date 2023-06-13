f9f9a7542fc5c59c644eba373100fe652818f6fb
taky-kongo <abdoulmalickkongo@gmail.com>
2023-06-13 17:25:03 +0000

    Update the file 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,24 +17,22 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	
-	line = (int**)malloc(sizeof(int*) * height);
-	/**
-	 * if (line == NULL)
-	 *	return (NULL);
-	 */
+	line = (int **)malloc(sizeof(int *) * height);
+	if (line == NULL)
+		return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
-		line[i] = (int*)malloc(sizeof(int) * width);
+		line[i] = (int *)malloc(sizeof(int) * width);
 		for (j = 0; j < width; j++)
 		{
-			line[j] = 0;
+			line[i][j] = 0;
 		}
 	}
-
-
-	for (i = 0; i < height; i++)
-		free(line[i]);
-	free(line);
 	return (line);
+	for (i = 0; i < height; i++)
+	{
+		free(line[i]);
+	}
+	free(line);
 }
