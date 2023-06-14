c4b08003944de164bf3427d4ffa7e3e9efd53333
taky-kongo <abdoulmalickkongo@gmail.com>
2023-06-14 17:33:07 +0000

    Add the file 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name, followed by a new line.
+ *
+ * @argc: the size of the array
+ * @argv: the array of arguments
+ *
+ * Return: Always 0.
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
