fd7bbc8a8ad491b5d11a1c162de7a8851b38728c
taky-kongo <abdoulmalickkongo@gmail.com>
2023-06-19 23:26:49 +0000

    Update the file 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,11 +10,8 @@
  * @owner: The owner of the dog
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
-{
-	struct dog *g;
-
-	g = d;	
-	g->name = name;
-	g->age = age;
-	g->owner = owner;
+{	
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
