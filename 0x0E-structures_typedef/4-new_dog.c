1e64e3ec28e4991a1da95dba1226aef84c109d1e
taky-kongo <abdoulmalickkongo@gmail.com>
2023-06-20 14:18:52 +0000

    Update the file 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,6 +14,7 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new;
+	int i = 0, j = 0;
 
 	new = malloc(sizeof(dog_t));
 	if (new == NULL)
@@ -22,6 +23,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
+	while (name[i] != '\0')
+		i++;
+	while (owner[j] != '\0')
+		j++;
+	new->name = malloc(sizeof(char) * i);
+	if (new->name == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->owner = malloc(sizeof(char) * j);
+	if (new->owner == NULL)
+	{
+		free(new->name);
+		free(new);
+	}
 	new->name = name;
 	new->age = age;
 	new->owner = owner;
