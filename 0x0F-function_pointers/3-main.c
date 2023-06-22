b5626ba23d81cebf5eb7f4c9e9de00abe326900a
taky-kongo <abdoulmalickkongo@gmail.com>
2023-06-22 19:01:56 +0000

    Update the file 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,10 +10,17 @@
  */
 int main(int argc, char **argv)
 {
+	int num1, num2;
+	char *operator;
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		return (98);
 	}
+	num1 = atoi(argv[2]);
+	operator = argv[3];
+	num2 = atoi(argv[4]);
+
 	return (0);
 }
