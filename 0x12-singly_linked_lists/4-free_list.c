8020da36eba8d579e2c2dcaffd076d3ca763118f
taky-kongo <abdoulmalickkongo@gmail.com>
2023-07-04 22:07:04 +0000

    Add the file 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,12 +1,18 @@
 #include "lists.h"
 
 /**
- *
- *
- *
- *
+ * free_list - frees a list_t list.
+ * @head: the list to enter
  */
 void free_list(list_t *head)
 {
-	free(head);
+	list_t *temp;
+
+	temp = head;
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		free(head);
+		head = temp;
+	}
 }
