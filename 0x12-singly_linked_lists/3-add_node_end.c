4eb4c831f18e631648749df785a064d61f9b6058
taky-kongo <abdoulmalickkongo@gmail.com>
2023-07-05 14:10:35 +0000

    Update the file 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -16,7 +16,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
-	
+
 	new_str = strdup(str);
 	if (new_str == NULL)
 	{
