84ec89f11b09ffbada1c17d010bbbe85857bbc7f
taky-kongo <abdoulmalickkongo@gmail.com>
2023-07-05 15:26:07 +0000

    Update the file 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,18 +10,22 @@ void free_list(list_t *head)
 
 	if (head == NULL)
 	{
-
+		return;
 	}
 	if (head->next == NULL)
 	{
+		free(head->str);
 		free(head);
 		head = NULL;
+		return;
 	}
-	while (temp != NULL)
+	while (head != NULL)
 	{
 		temp = head;
 		head = head->next;
+		free(temp->str);
 		free(temp);
 	}
 	free(head);
+	head = NULL;
 }
