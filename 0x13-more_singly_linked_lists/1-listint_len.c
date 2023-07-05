fdae8d7bb4e3dbb28785611a92340e02220d1673
taky-kongo <abdoulmalickkongo@gmail.com>
2023-07-05 11:31:36 +0000

    Add the file 1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked listint_t list.
+ * @h: the list linked to enter
+ *
+ * Return: the number of elements in a linked listint_t list.
+ */
+size_t listint_len(const listint_t *h)
+{
+	unsigned int count = 0;
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
