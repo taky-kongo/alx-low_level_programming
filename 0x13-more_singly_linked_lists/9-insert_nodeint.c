0c6ddac3e89716092e48b3451043c4577e889889
taky-kongo <abdoulmalickkongo@gmail.com>
2023-07-05 23:42:32 +0000

    Update the file 9-inser_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		if (i == idx)
 		{
-			if (ptr->next = NULL)
+			if (ptr->next == NULL)
 			{
 				ptr = new_node;
 			}
