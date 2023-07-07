dafd1f75f8a03f7e019a5d1fe81d96f8ffd1868c
taky-kongo <abdoulmalickkongo@gmail.com>
2023-07-07 23:08:52 +0000

    Update the file 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -18,7 +18,7 @@ int get_bit(unsigned long int n, unsigned int index)
 		count++;
 		number = number >> 1;
 	}
-	if (index > count -1)
+	if (index > (count - 1))
 		return (-1);
 	number = n;
 	j = count - 1;
