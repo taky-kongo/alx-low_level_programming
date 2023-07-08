9fda7004cc0cdd2bcf63de36f15e800e5844aac8
taky-kongo <abdoulmalickkongo@gmail.com>
2023-07-08 16:33:22 +0000

    Update te file 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,40 +10,16 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int temp;
-	unsigned int count, i;
-	int count_j = 0, j;
+	unsigned int count;
 
 	temp = *n;
 	while (*n > 0)
 	{
-		count_j++;
 		count++;
 		*n = *n >> 1;
 	}
 	*n = temp;
 
-	if ((count - 1) > index && (index < 32))
-	{
-		i = count - 1;
-		for (j = (count_j - 1); j >= 0; j--, i--)
-		{
-			if (i != index)
-				*n = ((*n >> i) & 1);
-			else
-				*n = (~(*n >> i) & 1);
-		}
-	}
-	else if ((count - 1) == index)
-	{
-		*n = ((*n >> index) & 1);
-		for (j = (int)(index - 1); j >= 0; j--)
-			*n = ((*n >> j) & 1);
-	}
-	else
-	{
-		*n = (~(*n >> index) & 1);
-		for (j = (int)(index - 1); j >= 0; j--)
-			*n = ((*n >> j) & 1);
-	}
+	*n = ((1 << index) ^ *n);
 	return (1);
 }
