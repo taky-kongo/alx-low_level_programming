a0d375bd59fec384e4b5e6b6c58a0c8fff198bd1
taky-kongo <abdoulmalickkongo@gmail.com>
2023-07-13 18:03:57 +0000

    Update the file 0-reaad_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,8 +19,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
-	if (fd < 0)
-		return (0);
+
 	read_int = read(fd, buffer, letters);
 	write(1, buffer, read_int);
 
