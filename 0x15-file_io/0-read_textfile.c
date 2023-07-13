60a2c714199098b0da6ddaa857afe41f5d847331
taky-kongo <abdoulmalickkongo@gmail.com>
2023-07-13 13:48:55 +0000

    Add the files of function 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+size_t read_textfile(const char *filename, size_t letters)
+{
+	if (filename == NULL)
+		return (0);
+	return (letters);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,9 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <stddef.h>
+
+int _putchar(char c);
+size_t read_textfile(const char *filename, size_t letters);
+
+#endif
