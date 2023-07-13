4539303198187d4769d5dcdfc12a7d552e53270c
taky-kongo <abdoulmalickkongo@gmail.com>
2023-07-13 19:01:02 +0000

    Update the file 0-reaad_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -23,7 +23,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
-		return (NULL);
+		return (0);
 	read_int = read(fd, buffer, letters);
 
 	write(STDOUT_FILENO, buffer, read_int);
