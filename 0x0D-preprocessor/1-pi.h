77247044297c4c0935ef046ef953d7c557b34fd1
taky-kongo <125483198+taky-kongo@users.noreply.github.com>
2023-08-01 19:49:04 +0000

    Delete 0x0D-preprocessor directory

--- 0x0D-preprocessor/1-pi.h
+++
@@ -1,6 +0,0 @@
-#ifndef __PI_H__
-#define __PI_H__
-
-#define PI 3.14159265359
-
-#endif
--- 0x0D-preprocessor/3-function_like_macro.h
+++
@@ -1,6 +0,0 @@
-#ifndef __FUNCTION_H__
-#define __FUNCTION_H__
-
-#define ABS(x) ((x) > 0 ? (x) : -(x))
-
-#endif
