a9b39d4e66b5332cc2569bf34731cf9b033c8a7c
taky-kongo <abdoulmalickkongo@gmail.com>
2023-08-08 19:35:20 +0000

    Update the file 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,7 +17,10 @@ int **alloc_grid(int width, int height)
 
 	tab = malloc(sizeof(int *) * height);
 	if (tab == NULL)
+	{
+		free(tab);
 		return (NULL);
+	}
 	for (i = 0; i < height; i++)
 		tab[i] = malloc(sizeof(int) * width);
 	for (i = 0; i < height; i++)
