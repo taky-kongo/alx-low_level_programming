0f5ef3a83db2d61f9e3b8edf25fcf16e29b3a4aa
taky-kongo <abdoulmalickkongo@gmail.com>
2023-08-18 14:13:03 +0000

    add the file 3-op_functions.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,13 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds a and b
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
