8c427756a6c0006cd939bbdae9c9d05d3c6276b8
taky-kongo <abdoulmalickkongo@gmail.com>
2023-08-18 14:24:57 +0000

    Update the file 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -11,3 +11,51 @@ int op_add(int a, int b)
 {
 	return (a + b);
 }
+
+/**
+ * op_sub - subtract a and b
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies a and b
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division of a and b
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the result of the division of a by b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo of a and b
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the remainder of the division of a by b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
