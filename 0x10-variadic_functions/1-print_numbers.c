5295fab2fe9cfcb15581aaae7f734f43c45bca25
taky-kongo <abdoulmalickkongo@gmail.com>
2023-08-22 18:33:35 +0000

    Update the file 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,7 +12,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(args, n);
 	if (n == 0)
-		return (0);
+		return;
 	for (i = 0; i < n; i++)
 	{
 		x = va_arg(args, unsigned int);
