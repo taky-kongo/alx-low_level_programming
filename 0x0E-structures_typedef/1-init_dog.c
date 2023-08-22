e2afa8ab42bc8a3af03e2648db56058652e03bf0
taky-kongo <abdoulmalickkongo@gmail.com>
2023-08-22 10:40:24 +0000

    Update the file 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,12 +2,15 @@
 
 /**
  * init_dog - initialize a variable of type struct dog
+ * @d: the struct to initialize
  * @name: the name of the dog
  * @age: the age of the dog
  * @owner: the owner of the dog
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
