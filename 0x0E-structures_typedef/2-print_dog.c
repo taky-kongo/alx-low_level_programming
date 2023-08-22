dff8bbc42a1e20eff486bb0db818a7a795392d48
taky-kongo <abdoulmalickkongo@gmail.com>
2023-08-22 11:12:55 +0000

    Update the file 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,12 +12,15 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	if (d->name == NULL)
-		printf("(nil)");
+		printf("Name: (nil)");
 	else
-		printf("%s\n", d->name);
-	printf("%f\n", d->age);
+		printf("Name: %s\n", d->name);
+	if (d->age == 0)
+		printf("Age: %f\n", d->age);
+	else
+		printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
-		printf("(nil)");
+		printf("Owner: (nil)");
 	else
-		printf("%s\n", d->owner);
+		printf("Owner: %s\n", d->owner);
 }
