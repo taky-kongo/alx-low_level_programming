945262cf6766af496116bd797a8058bf044049c9
taky-kongo <abdoulmalickkongo@gmail.com>
2023-08-22 18:25:22 +0000

    Add the file 1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers, followed by a new line.
+ * @separator: the separator between the numbers
+ * @n: the number of number to print
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i, x;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		x = va_arg(args, unsigned int);
+		printf("%d", x);
+		if (i != n - 1)
+		{
+			if (separator != NULL)
+				printf("%s", separator);
+		}
+		else
+			printf("\n");
+	}
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,7 +2,10 @@
 #define __VARIADIC_FUNC_H__
 
 #include <stdarg.h>
+#include <stdio.h>
 
+void print_numbers(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
+
 
 #endif
