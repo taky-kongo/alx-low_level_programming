e2912b8f62e2d2c3fbca17c1e20d3dbd3e1b4c67
taky-kongo <abdoulmalickkongo@gmail.com>
2023-08-24 22:21:34 +0000

    Add the readme file and lists.h

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,24 @@
+#ifndef __LISTS_H__
+#define __LISTS_H__
+
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	int len;
+	struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+
+#endif
