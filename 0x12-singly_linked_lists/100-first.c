1f858cf3f8906a86b1b91bfafa8bbdacc5071733
taky-kongo <abdoulmalickkongo@gmail.com>
2023-08-25 11:31:10 +0000

    Add the file 100-first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void print_before(void) __attribute__((constructor));
+
+/**
+ * print_before - print before the main function
+ */
+void print_before(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
