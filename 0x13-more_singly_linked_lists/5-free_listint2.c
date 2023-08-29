1a95a3c4028798005415a52855c3a0ff21bfde14
taky-kongo <abdoulmalickkongo@gmail.com>
2023-08-29 21:47:57 +0000

    Update the file

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,12 +6,9 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp;
-
-	if (*head == NULL)
-		return;
 	while (*head != NULL)
 	{
+		listint_t *tmp;
 		tmp = (*head)->next;
 		free(*head);
 		(*head) = tmp;
